e99aded1c6520d51bce0e4ab0f2f282e30d8d0a8
Ahmed <a.jassim2959@gmail.com>
2025-06-29 11:10:33 +0300

    Create play, render, and getComputerChoice functions

--- js/app.js
+++ js/app.js
@@ -17,6 +17,29 @@ const resultDisplayEl = document.querySelector('#result-display');
 
 /*-------------------------------- Functions --------------------------------*/
 
+function getComputerChoice() {
+    // Generate a random index based on the choices array length
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    console.log('Random Index:', randomIndex);
+    // select the item from the array
+    return choices[randomIndex];
+}
 
+// Initialize the game state
+function play() {
+    console.log('computer choice:', computerchoice);
+    computerchoice = getComputerChoice();
+    console.log('computer choice:', computerchoice);
+    getComputerChoice();
+    // after updating the game state, render to html
+    render();
+}
+
+play();
+
+// Update our UI/html directly
+function render() {
+
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
