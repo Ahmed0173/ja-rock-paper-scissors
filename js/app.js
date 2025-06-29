cb7987e83e8cfe3b80576dd7229fce4a734407df
Ahmed <a.jassim2959@gmail.com>
2025-06-29 14:06:27 +0300

    reset buttton

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ const rockBtnEl = document.querySelector('#rock');
 const paperBtnEl = document.querySelector('#paper');
 const scissorsBtnEl = document.querySelector('#scissors');
 const resultDisplayEl = document.querySelector('#result-display');
+const resetBtnEl = document.querySelector('#reset');
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -39,7 +40,56 @@ play();
 
 // Update our UI/html directly
 function render() {
+    resultDisplayEl.textContent = `Computer chose ${computerchoice} and you chose ${playerchoice}.`;
+}
 
+function compare() {
+    // Compare player choice and computer choice
+    if (playerchoice === computerchoice) {
+        msg = 'It\'s a tie!';
+    } else if (
+        (playerchoice === 'rock' && computerchoice === 'scissors') ||
+        (playerchoice === 'paper' && computerchoice === 'rock') ||
+        (playerchoice === 'scissors' && computerchoice === 'paper')
+    ) {
+        msg = 'You win!';
+    } else {
+        msg = 'You lose!';
+    }
+    console.log(msg);
+    resultDisplayEl.textContent = `Computer chose ${computerchoice} and you chose ${playerchoice}. ${msg}`;
+}
+
+function reset() {
+    resultDisplayEl.textContent = 'Game reset. Please make a choice.';
+    playerchoice = '';
+    computerchoice = '';
+    msg = '';
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
+
+rockBtnEl.addEventListener('click', function() {
+    playerchoice = 'rock';
+    console.log('Player choice:', playerchoice);
+    play();
+    compare();
+});
+
+paperBtnEl.addEventListener('click', function() {
+    playerchoice = 'paper';
+    console.log('Player choice:', playerchoice);
+    play();
+    compare();
+});
+
+scissorsBtnEl.addEventListener('click', function() {
+    playerchoice = 'scissors';
+    console.log('Player choice:', playerchoice);
+    play();
+    compare();
+});
+
+resetBtnEl.addEventListener('click', function() {
+    reset();
+});
